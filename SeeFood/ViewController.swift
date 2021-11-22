4b602ddab7fd172ad09f72f610e363f8a40951d6
AndresGutierrez <c0dingw.dre@gmail.com>
2021-11-22 13:19:28 -0800

    add functionality to iba, present image using imagePicker, update pList to permit access.

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -20,9 +20,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePicker.sourceType = .camera
         imagePicker.allowsEditing = false
     }
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userSelectedPhoto = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userSelectedPhoto
+    }
+    }
+    
     @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
-        
+        present(imagePicker, animated: true, completion: nil)
     }
     
 }
