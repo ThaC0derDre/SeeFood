5c26df9d215ccd8b83ec89d92fa4db2c0f35d0a8
AndresGutierrez <c0dingw.dre@gmail.com>
2021-11-24 08:52:34 -0800

    Update code to work with iOS 13. Change app to point out what it identifies.

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -8,11 +8,13 @@
 import UIKit
 import Vision
 import CoreML
+import Social
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     let imagePicker = UIImagePickerController()
+    var classificationResults : [VNClassificationObservation] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,29 +23,30 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePicker.allowsEditing = false
     }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let userSelectedPhoto = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            imageView.image = userSelectedPhoto
-            guard let ciImage = CIImage(image: userSelectedPhoto) else {
-                fatalError("Error turning UI Image to CIImage")
+        if let image = info[.originalImage] as? UIImage {
+            
+            imageView.image = image
+            imagePicker.dismiss(animated: true, completion: nil)
+            guard let ciImage = CIImage(image: image) else {
+                fatalError("couldn't convert uiimage to CIImage")
             }
             detect(image: ciImage)
-    }
+        }
     }
     func detect (image: CIImage){
         guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
             fatalError("Error making CoreML model")
         }
         let request = VNCoreMLRequest(model: model) { request, error in
-            guard let results = request.results as? [VNClassificationObservation] else{
-                fatalError("Error processing image.")
+            guard let results = request.results as? [VNClassificationObservation],
+                let topResult = results.first
+                else {
+                    fatalError("unexpected result type from VNCoreMLRequest")
             }
-            if let firstResult = results.first{
-                if firstResult.identifier.contains("hotdog"){
-                    self.navigationItem.title = "Hotdog!"
-                }else{
-                    self.navigationItem.title = "Not Hotdog!"
-                }
-            }
+            
+            self.navigationItem.title = ("\(topResult.identifier.description)")
+            self.navigationController?.navigationBar.barTintColor = UIColor.green
+            self.navigationController?.navigationBar.isTranslucent = false
         }
         let handler = VNImageRequestHandler(ciImage: image)
         do{
@@ -54,8 +57,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
         present(imagePicker, animated: true, completion: nil)
     }
     
 }
 
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+    return input.rawValue
+}
